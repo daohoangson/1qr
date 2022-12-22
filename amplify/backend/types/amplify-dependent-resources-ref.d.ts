03ea9670c2f002ea35b0a91d46ed29d499d5bfb2
Dao Hoang Son <daohoangson@gmail.com>
2022-12-23 06:26:06 +0700

    `yarn add aws-amplify @aws-amplify/ui-react`

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,14 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "1qr": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    }
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
+import '@aws-amplify/ui-react/styles.css'
+import { Amplify } from 'aws-amplify'
+import awsmobile from '../src/aws-exports'
+
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+
+Amplify.configure({ ...awsmobile, ssr: true })
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
